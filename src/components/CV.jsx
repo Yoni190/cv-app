7b4619fa5b6a4a252a3cef95b118fe9f3c785763
Yoni190 <yfisuhu@gmail.com>
2026-03-24 23:32:59 +0300

    Display education experience

--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -6,6 +6,16 @@ const CV = ({ generalData, educationData, practicalData }) => {
         <h1>CV</h1>
         <h2>{generalData.firstName} {generalData.lastName}</h2>
         <p>{generalData.email} {generalData.phone && '|'} {generalData.phone}</p>
+
+
+        <h1>{educationData[0].schoolName && 'Education Experience'}</h1>
+        {educationData.map((data) => (
+            <div key={data.id}>
+                <h2>{data.schoolName}</h2>
+                <p>{data.studyTitle} {data.startDate} {data.endDate && '-'} {data.endDate}</p>
+            </div>
+        ))}
+        
     </div>
   )
 }
