afbd22bc75b45749c7f33a9e654badc36caa5e71
Yoni190 <yfisuhu@gmail.com>
2026-03-26 09:07:44 +0300

    Add styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
-import './App.css'
 import GeneralInfo from './components/GeneralInfo'
 import Education from './components/Education'
 import Practical from './components/Practical'
 import CV from './components/CV'
+import './styles/style.css'
 
 function App() {
   const currentDate = new Date().toISOString().slice(0,10)
@@ -44,10 +44,15 @@ function App() {
 
   return (
     <>
-      <GeneralInfo generalData={generalData} setGeneralData={setGeneralData} />
-      <Education educationData={educationData} setEducationData={setEducationData}/>
-      <Practical practicalData={practicalData} setPracticalData={setPracticalData}/>
-      <CV generalData={generalData} educationData={educationData} practicalData={practicalData}/>
+      <div className='container'>
+        <div>
+          <GeneralInfo generalData={generalData} setGeneralData={setGeneralData} />
+          <Education educationData={educationData} setEducationData={setEducationData}/>
+          <Practical practicalData={practicalData} setPracticalData={setPracticalData}/>
+        </div>
+
+        <CV generalData={generalData} educationData={educationData} practicalData={practicalData}/>
+      </div>
     </>
   )
 }
